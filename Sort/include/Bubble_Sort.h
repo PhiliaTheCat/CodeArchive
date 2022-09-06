504c7dcf91d2da495ecad13556edfd65771c5436
PhiliaTheCat <2787974051@qq.com>
2022-09-06 22:27:08 +0800

    Merge Sort Overwrited

--- Sort/include/Bubble_Sort.h
+++
@@ -1,4 +0,0 @@
-#ifndef __BUBBLE_SORT_H__
-    #define __BUBBLE_SORT_H__
-    void Bubble_Sort(int arr[], int l, int r, int (*judge)(int, int));
-#endif
--- Sort/include/Merge_Sort.h
+++
@@ -1,4 +0,0 @@
-#ifndef __MERGE_SORT_H__
-    #define __MERGE_SORT_H__
-    void Merge_Sort(int arr[], int l, int r, int (*judge)(int, int));
-#endif
