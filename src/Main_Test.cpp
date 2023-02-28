001d56148c354d69b702b404cdc7129af95eba2b
PhiliaTheCat <2787974051@qq.com>
2023-02-28 15:14:55 +0800

    Addition Expandsion

--- src/Main_Test.cpp
+++ src/Main_Test.cpp
@@ -8,5 +8,10 @@ using namespace ptc;
 
 int main()
 {
-    
+    High_Precision<int(1e5)> a;
+    cin >> a;
+    cout << ((+a) = a + 1) << endl;
+    cout << a + 12 << endl;
+    cout << 12 + a << endl;
+    cout << a + a << endl;
 }
