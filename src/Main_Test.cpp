50b00a75ce245ff5a9c7826118ba9147f4996e6c
PhiliaTheCat <2787974051@qq.com>
2024-02-04 13:17:54 +0800

    Binary Implementation

--- src/Main_Test.cpp
+++ src/Main_Test.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
 
-#include "../include/Sort/Quick_Sort"
+#include "../include/Math/High_Precision"
 
 using namespace std;
-using namespace ptc;
-
-inline bool cmp(const int &l, const int &r)
-{
-    return l < r;
-}
 
 int main()
 {
-    int arr[10] = 
-    {
-        1, 5, -11, 22, 13,
-        -7, -3, 1000, 2, -99999
-    };
-    Quick_Sort(arr, arr + 10, cmp);
-    for (int i = 0; i < 10; i += 1)
-        cout << arr[i] << endl;
+    ptc::High_Precision<> a = 114514;
+    cout << a << endl;
     return 0;
 }
