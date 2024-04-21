5d8055dcdc4f6ca7965398f966f156ee1a84e110
PhiliaTheCat <2787974051@qq.com>
2024-04-22 01:08:45 +0800

    Add GTest Support

---
+++ test/Modulus_Test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include "../include/Math/Modulus"
+
+using Target = ptc::Modulus<97>;
+
+TEST(Modulus_Test, HandlePositveInput)
+{
+    EXPECT_EQ(Target(114514), Target(114514 + 97 * 13));
+    EXPECT_NE(Target(13), Target(114514));
+}
+
+TEST(Modulus_Test, HandleNegativeInput)
+{
+    EXPECT_EQ(Target(-114514), Target(-(114514 + 97 * 13)));
+    EXPECT_NE(Target(-13), Target(-114514));
+}
