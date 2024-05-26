178ddc127bac20506a2d457208ae49079e4c8c8b
PhiliaTheCat <2787974051@qq.com>
2024-05-26 12:05:38 +0800

    Code style

--- test/Sort/MergeSort_Test.cpp
+++ test/Sort/MergeSort_Test.cpp
@@ -8,12 +8,12 @@ const auto &cmp = [](const int &l, const int &r) -> bool {return l < r;};
 TEST(MergeSort_Test, ShortNativeArray)
 {
     int arr[16] =
-    {
-        1, 3, 7, 9, 11,
-        12, 6, 0, 15, 14,
-        2, 8, 10, 13, 4,
-        5
-    };
+        {
+            1, 3, 7, 9, 11,
+            12, 6, 0, 15, 14,
+            2, 8, 10, 13, 4,
+            5
+        };
 
     ptc::MergeSort(arr, arr + 16, cmp);
 
@@ -24,12 +24,12 @@ TEST(MergeSort_Test, ShortNativeArray)
 TEST(MergeSort_Test, ShortVector)
 {
     std::vector<int, std::allocator<int>> arr =
-    {
-        1, 3, 7, 9, 11,
-        12, 6, 0, 15, 14,
-        2, 8, 10, 13, 4,
-        5
-    };
+        {
+            1, 3, 7, 9, 11,
+            12, 6, 0, 15, 14,
+            2, 8, 10, 13, 4,
+            5
+        };
 
     ptc::MergeSort(arr.begin(), arr.end(), cmp);
 
@@ -40,13 +40,13 @@ TEST(MergeSort_Test, ShortVector)
 TEST(MergeSort_Test, LongNativeArray)
 {
     int arr[40] =
-    {
-        2, 4, 5, 6, 7, 1, 11, 13,
-        12, 14, 54, 32, 22, 43, 21, 77,
-        96, 11, -114514, 114514, 23, -11, -22, 9,
-        -12, 97, 1919810, 88, 90, -9, -14, -13,
-        -77, -900, -1919810, -8, 111111, 222222, 33333, 44444444
-    };
+        {
+            2, 4, 5, 6, 7, 1, 11, 13,
+            12, 14, 54, 32, 22, 43, 21, 77,
+            96, 11, -114514, 114514, 23, -11, -22, 9,
+            -12, 97, 1919810, 88, 90, -9, -14, -13,
+            -77, -900, -1919810, -8, 111111, 222222, 33333, 44444444
+        };
 
     ptc::MergeSort(arr, arr + 40, cmp);
 
@@ -56,7 +56,7 @@ TEST(MergeSort_Test, LongNativeArray)
 
 TEST(MergeSort_Test, WorstCondition)
 {
-    int *arr = new int [int(1e7)];
+    int *arr = new int[int(1e7)];
     for (int i = 0; i < int(1e7); i += 1)
         arr[i] = int(5e6) - 0;
 
@@ -65,6 +65,6 @@ TEST(MergeSort_Test, WorstCondition)
     for (int i = 1; i < int(1e7); i += 1)
         EXPECT_TRUE(!cmp(arr[i], arr[i - 1]));
 
-    delete [] arr;
+    delete[] arr;
 }
 
