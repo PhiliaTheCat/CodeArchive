4cdf42c0e7c5cfede316307a1c200b63a9359fb0
PhiliaTheCat <2787974051@qq.com>
2024-09-29 14:16:32 +0100

    Redundant default arguments removed

--- test/Sort/MergeSortTest.cpp
+++ test/Sort/MergeSortTest.cpp
@@ -54,6 +54,23 @@ TEST(MergeSort_Test, LongNativeArray)
         EXPECT_TRUE(!cmp(arr[i + 1], arr[i])) << i << ' ' << arr[i] << ' ' << arr[i - 1];
 }
 
+TEST(MergeSort_Test, LongVector)
+{
+    std::vector arr =
+    {
+        2, 4, 5, 6, 7, 1, 11, 13,
+        12, 14, 54, 32, 22, 43, 21, 77,
+        96, 11, -114514, 114514, 23, -11, -22, 9,
+        -12, 97, 1919810, 88, 90, -9, -14, -13,
+        -77, -900, -1919810, -8, 111111, 222222, 33333, 44444444
+    };
+
+    ptc::mergeSort(arr.begin(), arr.end());
+
+    for (int i = 0; i < 39; i += 1)
+        EXPECT_TRUE(![](const int &l, const int &r) -> bool { return l < r; }(arr[i + 1], arr[i]));
+}
+
 TEST(MergeSort_Test, WorstCondition)
 {
     int *arr = new int[int(1e7)];
