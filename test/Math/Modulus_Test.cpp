9dde763f0ecbf340895d7a90b6ec838df6fd3351
PhiliaTheCat <2787974051@qq.com>
2024-10-17 16:18:04 +0100

    Fix value catagory for Modulus

--- test/Math/Modulus_Test.cpp
+++ test/Math/Modulus_Test.cpp
@@ -18,11 +18,12 @@ TEST(Modulus_Test, IsStandardLayout)
 TEST(Modulus_Test, PositveConstructor)
 {
     EXPECT_EQ(Target(114514), Target(114514 + 97 * 13));
-    EXPECT_NE(Target(13), Target(114514));
+    EXPECT_EQ(Target(359), Target(359 + 97 * 1));
+    EXPECT_EQ(Target(62), Target(62 + 97 * 11));
 }
 
 TEST(Modulus_Test, NegativeConstructor)
 {
     EXPECT_EQ(Target(-114514), Target(-(114514 + 97 * 13)));
-    EXPECT_NE(Target(-13), Target(-114514));
+    EXPECT_EQ(-Target(-54), Target(114514));
 }
