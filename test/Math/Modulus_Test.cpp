4ef8cb2ff2ad3b09fa03486c4d1b4dc830d0b6b0
PhiliaTheCat <2787974051@qq.com>
2024-10-18 10:22:57 +0100

    Optimize code style

--- test/Math/Modulus_Test.cpp
+++ test/Math/Modulus_Test.cpp
@@ -21,3 +21,10 @@ TEST(Modulus_Test, PositveConstructor)
     EXPECT_EQ(Target(359), Target(359 + 97 * 1));
     EXPECT_EQ(Target(62), Target(62 + 97 * 11));
 }
+
+TEST(Modulus_Test, NegativeConstructor)
+{
+    EXPECT_EQ(Target(-11451), Target(-11451 + 97 * 13));
+    EXPECT_EQ(Target(-3594), Target(-3594 + 97 * 1));
+    EXPECT_EQ(Target(-6), Target(6 + 97 * 11));
+}
